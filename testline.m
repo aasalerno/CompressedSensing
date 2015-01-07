260e453354bc021f0d5d852d40cb3f7c75a6459d
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-06 21:44:06 -0500

    Code written originally for my committee meeting. Used to make a "filter" of sorts to bring zero values for anything that is outside of a certain distance from the value that we're looking for.

---
+++ testline.m
@@ -0,0 +1,43 @@
+function testline(data,slp)
+%data = rand(200,200);
+s = size(data);
+%slp = 0;
+[x,y] = meshgrid(linspace(-1,1,s(1)),linspace(-1,1,s(2)));
+sampFac = 0.5;
+wid = 0.5*sampFac;
+
+if ~isnan(slp)
+    lnpts = [[0 0 0];[1,slp,0]];
+end
+
+
+for i = 1:s(1)
+    for j = 1:s(2)
+        if isnan(slp)
+            if abs(x(i,j)) < wid || data(i,j) < sampFac
+                data(i,j) = 1;
+            else 
+                data(i,j) = 0;
+            end
+            
+        else
+            if point_to_line([x(i,j),y(i,j),0],lnpts(1,:),lnpts(2,:)) < wid
+            %if sqrt(x(i,j)^2 + y(i,j)^2) < wid || data(i,j) < sampFac
+                data(i,j) = 1;
+            else
+                data(i,j) = 0;
+            end
+        end
+    end
+end
+
+a = imagesc(flipud(data));
+axis off
+colormap(gray)
+xlabel('k_x')
+ylabel('k_y')
+
+% 
+% diff = sum(data(:))/prod(size(data));
+% 
+% disp(['The actual undersample factor is ',num2str(diff*100),'%.'])
