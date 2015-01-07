0d3fcc7b01e95d5dfd628a6dfd932ed32bd571f6
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-06 21:46:17 -0500

    Added some code to allow us to change the order for the gradient vector in order to match the data. For example, if the data is in zyx, it switches the gradient to zyx from xyz.

--- undersamp.m
+++ undersamp.m
@@ -1,4 +1,4 @@
-function undersamp(filename,outname,rodir)
+function undersamp(filename,outname,sampFac,rodir)
 % This function is made to pseudoundersample a dataset as we wish to do
 % so.
 %
@@ -25,14 +25,18 @@ function undersamp(filename,outname,rodir)
 %         changed - that is, undersampled
 % rodir - readout direction (string)
 %       - The point of this data is to effectively make sure that we are
-%       not messing up the data that we are looking at. 
+%       not messing up the data that we are looking at.
 %       - As per Jacob, the general readout direction is 'y' for exvivo
 %       scans
 %
 %
 % Anthony Salerno                       01/06/15
 
-if nargin<3
+if nargin < 3
+    sampFac = 0.5;
+end
+
+if nargin < 4
     rodir = 'y';
 end
 
@@ -49,10 +53,57 @@ for i = 1:length(dimorder)
     dim{i} = a(1);
     clear a
 end
-
 clear dimorder
+% Now we look at the order and see how we have to change around our
+% gradient directions so that they match!
+loc = zeros(1,3);
+for i=1:3
+    if dim{i} == 'x'
+        loc(i) = 1;
+    elseif dim{i} == 'y'
+        loc(i) = 2;
+    elseif dim{i} == 'z'
+        loc(i) = 3;
+    else
+        error('Problem with your dimorder. Check file')
+    end
+end
 
+% Change the order of the gradient vectors so it corresponds with the data
+graddir = load('GradientVector.txt');
+graddir = [graddir(:,loc(1)) graddir(:,loc(2)) graddir(:,loc(3))];
 
+% Get which gradient we're working with by Splitting the name
+nameSpl = strsplit(filename);
+gradvec = graddir(str2num(nameSpl{end-1}),:); % Get the gradient vector so we know which one we need
 
-
+if gradvec > 30
+    disp('No gradient. Do not undersample');
+else
+    readloc = find(ismember(dim,rodir)); % This tells us which dimension the readout is on for our dataset, i.e. 1, 2, or 3
+    gradvec = find(~ismember(gradvec,gradvec(readloc))); % Gives us only the values for the non readout direction
+    % Quick and dirty way
+    % to do a projection
     
+    % Here is where we will have the heart of the code - this is where we will
+    % actually do the undersampling
+    % As of 01/06/15 the method will only have the linear undersampling as the
+    % perpendicular value of the gradient vector
+    
+    gradvec = [-gradvec(2) gradvec(1)]; % Here is where we make it perpendicular
+    slp = gradvec(2)/gradvec(1);
+    n = size(rawdata);
+    
+    if readloc == 1
+        slicesz = ones(n(2),n(3)); %What is the size of each slice
+        filt = testline(slicesz,slp); % Make the filter that we will use
+        
+        for i = 1:n(1)
+            data(i,:,:) = reshape(filt,size(rawdata(i,:,:))).*rawdata(i,:,:);
+        end
+    end
+    
+    
+    
+end
+
