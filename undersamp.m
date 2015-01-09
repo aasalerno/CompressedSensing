258be11c5a26413a3c010940da9056439554a88a
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-09 14:00:08 -0500

    Added functionality to use genFilt.m, hasn't been tested yet

--- undersamp.m
+++ undersamp.m
@@ -108,39 +108,37 @@ gradvec = graddir(round(str2num(nameSpl{end-1})),:); % Get the gradient vector s
 if gradvec > 30
     disp('No gradient. Do not undersample');
 else
-    readloc = find(ismember(dim,gvdir)); % This tells us which dimension the readout is on for our dataset, i.e. 1, 2, or 3
-    tester = 1:3;
-    tester = find(~ismember(tester,readloc));
-    gradvec = gradvec(tester); % Gives us only the values for the non readout direction
-    % Quick and dirty way to do a projection
-    
-    % Here is where we will have the heart of the code - this is where we will
-    % actually do the undersampling
-    % As of 01/06/15 the method will only have the linear undersampling as the
-    % perpendicular value of the gradient vector
-    
-    gradvec = [-gradvec(2) gradvec(1)]; % Here is where we make it perpendicular
-    slp = gradvec(2)/gradvec(1);
-    n = size(rawdata);
-    
+%     readloc = find(ismember(dim,gvdir)); % This tells us which dimension the readout is on for our dataset, i.e. 1, 2, or 3
+%     tester = 1:3;
+%     tester = find(~ismember(tester,readloc));
+%     gradvec = gradvec(tester); % Gives us only the values for the non readout direction
+%     % Quick and dirty way to do a projection
+%     
+%     % Here is where we will have the heart of the code - this is where we will
+%     % actually do the undersampling
+%     % As of 01/06/15 the method will only have the linear undersampling as the
+%     % perpendicular value of the gradient vector
+%     
+%     gradvec = [-gradvec(2) gradvec(1)]; % Here is where we make it perpendicular
+%     slp = gradvec(2)/gradvec(1);
+%     n = size(rawdata);
+    filt = genFilt(type,data,filename,sampFac,loc,gvdir);
     if readloc == 1
-        slicesz = ones(n(2),n(3)); %What is the size of each slice
-        filt = testline(slicesz,slp,sampFac); % Make the filter that we will use
-        filt = uint16(filt);
+
         for i = 1:n(1)
             data(i,:,:) = reshape(filt,size(rawdata(i,:,:))).*rawdata(i,:,:); % Applies the filter to each "slice"
         end
     elseif readloc == 2
-        slicesz = ones(n(1),n(3)); %What is the size of each slice
-        filt = testline(slicesz,slp,sampFac); % Make the filter that we will use
+%         slicesz = ones(n(1),n(3)); %What is the size of each slice
+%         filt = testline(slicesz,slp,sampFac); % Make the filter that we will use
         filt = uint16(filt);
         
         for i = 1:n(2)
             data(:,i,:) = reshape(filt,size(rawdata(:,i,:))).*rawdata(:,i,:); % Applies the filter to each "slice"
         end
     elseif readloc == 3
-        slicesz = ones(n(1),n(2)); %What is the size of each slice
-        filt = testline(slicesz,slp,sampFac); % Make the filter that we will use
+%         slicesz = ones(n(1),n(2)); %What is the size of each slice
+%         filt = testline(slicesz,slp,sampFac); % Make the filter that we will use
         filt = uint16(filt);
         
         for i = 1:n(3)
