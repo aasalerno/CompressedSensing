2e96a9a67718a85673ff144f8b2fc5e55964fa09
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-12 18:16:43 -0500

    Added funcionality to handle datasets! Checked if it works the same as when using the file and it seems to work well. In addition added a few lines to make sure that the max data and min data were put in the correct order.

--- InputOutput/mincmap.m
+++ InputOutput/mincmap.m
@@ -1,4 +1,4 @@
-function mapdata = mincmap(dataname)
+function mapdata = mincmap(dataset,datamin,datamax,ind)
 % mapdata = mincmap(filename)
 %
 %
@@ -10,39 +10,64 @@ function mapdata = mincmap(dataname)
 %
 % It will first be built in a brute force method, and then, it will be
 % optimized to work more quickly and efficiently
+% 
+% dataset - the data or filename
+% datamin - minima from the minc file
+% datamax - maxima from minc files
+% ind - the index that the minima/maxima are found on (generally the slice
+%       dimension)
 
 % Read in some data
-data = mincread(dataname,'image');
-datamax = mincread(dataname,'max');
-datamin = mincread(dataname,'min');
-N = size(data);
-mapdata = zeros(N);
-n = size(datamax);
-disp('Image, max, and min read in. Second matrix made to be altered for output');
 
-% Double check to make sure that the order is RO, PE, SL
-check = h5readatt(dataname,'/minc-2.0/info/vnmr','array');
-check = check(2:end-2); % This gets rid of the brackets at the beginning and end
-check = strsplit(check,',');
-refStr = 'dsl'; 
-ind = strfind(check,refStr);
-ind = find(~cellfun(@isempty,ind));
-disp('Dimension of the slice found.')
+% To halt errors
+if nargin == 1
+    datamax = 0;
+    datamin = 0;
+end
+
+% if somehow a mistake was made, we fix it here.
+if datamin(1) > datamax(1)
+    a = datamin;
+    datamin = datamax;
+    datamax = a;
+end
+
+if isa(dataset,'string') || isa(dataset,'char')
+    filename = dataset;
+    dataset = mincread(filename,'image');
+    datamax = mincread(filename,'max');
+    datamin = mincread(filename,'min');
+    N = size(dataset);
+    mapdata = zeros(N);
+    n = size(datamax);
+    disp('Image, max, and min read in. Second matrix made to be altered for output');
+    
+    % Double check to make sure that the order is RO, PE, SL
+    check = h5readatt(filename,'/minc-2.0/info/vnmr','array');
+    check = check(2:end-2); % This gets rid of the brackets at the beginning and end
+    check = strsplit(check,',');
+    refStr = 'dsl';
+    ind = strfind(check,refStr);
+    ind = find(~cellfun(@isempty,ind));
+    disp('Dimension of the slice found.')
+else
+     n = size(datamax);
+end
 
 
 if ind == 1
     for i = 1:n
         dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(i,:,:) = data(i,:,:)*dy + datamin(i);
+        mapdata(i,:,:) = dataset(i,:,:)*dy + datamin(i);
     end
 elseif ind == 2
     for i = 1:n
         dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(:,i,:) = data(:,i,:)*dy + datamin(i);
+        mapdata(:,i,:) = dataset(:,i,:)*dy + datamin(i);
     end
 elseif ind == 3
     for i = 1:n
         dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(:,:,i) = data(:,:,i)*dy + datamin(i);
+        mapdata(:,:,i) = dataset(:,:,i)*dy + datamin(i);
     end
 end
