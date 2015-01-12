c4ea0a9339018217185c47ef55e77ea99f1f4bb5
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-12 14:41:54 -0500

    Fixed mistake on line 15 where strcmp was with max instead of min

--- InputOutput/mincread.m
+++ InputOutput/mincread.m
@@ -3,17 +3,19 @@ function data = mincread(filename,type)
 %
 % The sole purpose of this code is to get the image data or max/min data
 % from a minc file
-
-if nargin<2
-    type = 'image';
-end
+% 
+% if nargin < 2
+%     type = 'image';
+% end
+type
 
 if strcmp(type,'image')
     data = h5read(filename,'/minc-2.0/image/0/image');
 elseif strcmp(type,'max')
     data = h5read(filename,'/minc-2.0/image/0/image-max');
-elseif strcmp(type,'max')
+elseif strcmp(type,'min')
     data = h5read(filename,'/minc-2.0/image/0/image-max');
 else
+    data = 0;
     disp('Type not understood. Retry with either ''image'', ''max'', or ''min''');
 end
