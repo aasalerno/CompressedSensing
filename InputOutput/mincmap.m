3fb6c9e086ed277daba1766d4684bd66050cbe67
aasalerno <anthony.salerno@mouseimaging.com>
2015-01-12 18:26:20 -0500

    Added a conditional to make sure that this code is only used on the correct data (i.e. not being used on doubles)

--- InputOutput/mincmap.m
+++ InputOutput/mincmap.m
@@ -10,7 +10,7 @@ function mapdata = mincmap(dataset,datamin,datamax,ind)
 %
 % It will first be built in a brute force method, and then, it will be
 % optimized to work more quickly and efficiently
-% 
+%
 % dataset - the data or filename
 % datamin - minima from the minc file
 % datamax - maxima from minc files
@@ -32,42 +32,46 @@ if datamin(1) > datamax(1)
     datamax = a;
 end
 
-if isa(dataset,'string') || isa(dataset,'char')
-    filename = dataset;
-    dataset = mincread(filename,'image');
-    datamax = mincread(filename,'max');
-    datamin = mincread(filename,'min');
-    N = size(dataset);
-    mapdata = zeros(N);
-    n = size(datamax);
-    disp('Image, max, and min read in. Second matrix made to be altered for output');
+if isa(dataset,'uint16') || isa(dataset,'string') || isa(dataset,'char')
+    if isa(dataset,'string') || isa(dataset,'char')
+        filename = dataset;
+        dataset = mincread(filename,'image');
+        datamax = mincread(filename,'max');
+        datamin = mincread(filename,'min');
+        N = size(dataset);
+        mapdata = zeros(N);
+        n = size(datamax);
+        disp('Image, max, and min read in. Second matrix made to be altered for output');
+        
+        % Double check to make sure that the order is RO, PE, SL
+        check = h5readatt(filename,'/minc-2.0/info/vnmr','array');
+        check = check(2:end-2); % This gets rid of the brackets at the beginning and end
+        check = strsplit(check,',');
+        refStr = 'dsl';
+        ind = strfind(check,refStr);
+        ind = find(~cellfun(@isempty,ind));
+        disp('Dimension of the slice found.')
+    else
+        n = size(datamax);
+    end
     
-    % Double check to make sure that the order is RO, PE, SL
-    check = h5readatt(filename,'/minc-2.0/info/vnmr','array');
-    check = check(2:end-2); % This gets rid of the brackets at the beginning and end
-    check = strsplit(check,',');
-    refStr = 'dsl';
-    ind = strfind(check,refStr);
-    ind = find(~cellfun(@isempty,ind));
-    disp('Dimension of the slice found.')
+    
+    if ind == 1
+        for i = 1:n
+            dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
+            mapdata(i,:,:) = dataset(i,:,:)*dy + datamin(i);
+        end
+    elseif ind == 2
+        for i = 1:n
+            dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
+            mapdata(:,i,:) = dataset(:,i,:)*dy + datamin(i);
+        end
+    elseif ind == 3
+        for i = 1:n
+            dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
+            mapdata(:,:,i) = dataset(:,:,i)*dy + datamin(i);
+        end
+    end
 else
-     n = size(datamax);
-end
-
-
-if ind == 1
-    for i = 1:n
-        dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(i,:,:) = dataset(i,:,:)*dy + datamin(i);
-    end
-elseif ind == 2
-    for i = 1:n
-        dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(:,i,:) = dataset(:,i,:)*dy + datamin(i);
-    end
-elseif ind == 3
-    for i = 1:n
-        dy = (datamax(i)-datamin(i))/(2^16); %Overall difference between max and min over the number of points
-        mapdata(:,:,i) = dataset(:,:,i)*dy + datamin(i);
-    end
+    disp('Wrong datatype. If you have a double, use mincmap16 to convert to uint16.')
 end
