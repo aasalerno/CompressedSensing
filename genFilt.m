72db143e84ca3363e846b65d6c45898dc6f8b0dc
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-01-15 12:56:31 -0500

    Made 'showit = 0' so that we don't get overloaded with figures

--- genFilt.m
+++ genFilt.m
@@ -1,21 +1,28 @@
-function [fil,readloc] = genFilt(type,data,filename,sampFac,loc,gvdir)
+function [fil,readloc] = genFilt(filttype,data,filename,sampFac,loc,gvdir)
 % This function is built to generate the filter for undersampling. As it
 % stands, it will zero fill the rest instead of trying to put some
 % randomness to it (that is, there is no data except for where we put this
 % filter) - it is NOT a PDF.
 %
 % Anthony Salerno
-showit = 1;
+showit = 0;
 graddir = load('GradientVector.txt');
 graddir = [graddir(:,loc(1)) graddir(:,loc(2)) graddir(:,loc(3))];
 
 % Get which gradient we're working with by Splitting the name
 nameSpl = strsplit(filename,'.');
-gradvec = graddir(round(str2num(nameSpl{end-1})),:); % Get the gradient vector so we know which one we need
+tester=[];
+for i = 1:numel(nameSpl)
+    numval = str2num(nameSpl{i});
+    if ~isempty(numval)
+        tester = [tester numval];
+    end
+end
 
-if gradvec > 30
+if tester(end) > 30
     disp('No gradient. Do not undersample');
 else
+    gradvec = graddir(tester(end),:); % Get the gradient vector so we know which one we need
     readloc = find(ismember('dro',gvdir)); % This tells us which dimension the readout is on for our dataset, i.e. 1, 2, or 3
     tester = 1:3;
     tester = find(~ismember(tester,readloc));
@@ -26,7 +33,7 @@ else
     % actually do the undersampling
     % As of 01/06/15 the method will only have the linear undersampling as the
     % perpendicular value of the gradient vector
-    if strcmp('per',type)
+    if strcmp('per',filttype)
         gradvec = [-gradvec(2) gradvec(1)]; % Here is where we make it perpendicular
         disp('Creating a "perpendicular to gradient direction" filter')
     else
@@ -42,11 +49,11 @@ else
         
         if readloc == 1
             slicesz = ones(n(2),n(3)); %What is the size of each slice
-            if strcmp('per',type) || strcmp('par',type)
+            if strcmp('per',filttype) || strcmp('par',filttype)
                 fil = linefilt(slicesz,slp,sampFac); % Make the filter that we will use
-            elseif strcmp(type,'lores')
+            elseif strcmp(filttype,'lores')
                 fil = sqfilt(slicesz,sampFac);
-            elseif strcmp(type,'circ')
+            elseif strcmp(filttype,'circ')
                 fil = circfilt(slicesz,sampFac);
             end
             
