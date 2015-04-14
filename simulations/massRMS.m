8bc64db3d5c8b931f7b20772f256816b40402768
aasalerno <anthony.salerno@mouseimaging.com>
2015-04-14 09:39:40 -0400

    Function to do an RMS for many different types simultaneously

--- simulations/massRMS.m
+++ simulations/massRMS.m
@@ -16,6 +16,9 @@ diffs = zeros(1,length(dirNames));
 
 ref = ['Full' typ 'Circ'];
 
+tmploc = find(strcmp('Full',dirNames));
+dirNames = circshift(dirNames,[0,tmploc]);
+
 for i = 1:length(dirNames)
     cmp = [dirNames{i} typ dirNames{i}];
     if strcmp(dirNames{i},'Full')
@@ -26,5 +29,3 @@ end
 
 bar(diffs);
 set(gca,'XTickLabel',dirNames)
-
-    
