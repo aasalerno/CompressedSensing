28d8486dc29c1656f2822e3219f6e34475b4160f
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-05-12 11:51:27 -0400

    First commit for this code. Seems to be working (was tested quickly) -- unfortunately takes about 8 seconds to run, but for loops cannot be avoided for this

--- grads/gDir.m
+++ grads/gDir.m
@@ -1,5 +1,30 @@
 function gradDir = gDir(x,params)
 % computes the gradient of data similarity
+tic
+
+dirPair = params.dirPair;
+wgt = params.dirPairWeight;
+
+N = size(dirPair,1);
+gradDirTot = zeros([size(x,1) size(x,2) N]);
+gradDir = zeros(size(x));
+
+% This takes 5 seconds :'(
+
+for kk = 1:N
+    i = dirPair(kk,1);
+    j = dirPair(kk,2);
+    x1 = squeeze(x(:,:,i));
+    x2 = squeeze(x(:,:,j));
+    gradDirTot(:,:,kk) = params.XFM*(2.*wgt(kk).*(params.XFM'*x1-params.XFM'*x2));
+end
 
 
-%gradDir = 2*(
+nums = (1:N)';
+
+for kk = 1:30
+    inRow = nums(any(kk == dirPair,2).*nums ~= 0);
+    gradDir(:,:,kk) = sum(gradDirTot(:,:,inRow),3);
+end
+toc
+% Whole thing takes ~8 seconds.
