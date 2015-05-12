de4bbb80de94b75a523cfa87c03892f89646b53c
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-05-12 10:35:11 -0400

    The code was altered to allow for 3D data -- and should still maintain 2D data well.

--- grads/gOBJ.m
+++ grads/gOBJ.m
@@ -1,6 +1,15 @@
 function gradObj = gOBJ(x,params)
 % computes the gradient of the data consistency
 
-	gradObj = params.XFM*(params.FT'*(params.FT*(params.XFM'*x) - params.data));
+if length(size(x)) == 2
+    x = reshape(x,[size(x) 1]);
+end
 
-gradObj = 2*gradObj ;
+gradObj = zeros(size(x));
+
+for kk = 1:size(x,3)
+    x1 = squeeze(x(:,:,kk));
+    dat = squeeze(params.data(:,:,kk));
+    gradObj(:,:,kk) = params.XFM*(params.FT{kk}'*(params.FT{kk}*(params.XFM'*x1) - dat));
+    gradObj(:,:,kk) = 2*gradObj(:,:,kk) ;
+end
