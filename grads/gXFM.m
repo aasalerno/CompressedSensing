0ef37117dd3d92b9888ebfdd4cecc67f55a3d972
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-05-12 11:55:10 -0400

    Added in functionality to take 3D data and run for each slice

--- grads/gXFM.m
+++ grads/gXFM.m
@@ -3,4 +3,9 @@ function grad = gXFM(x,params)
 
 p = params.pNorm;
 
-grad = p*x.*(x.*conj(x)+params.l1Smooth).^(p/2-1);
+grad = zeros(size(x));
+
+for kk = 1:size(x,3)
+    x1 = squeeze(x(:,:,kk));
+    grad(:,:,kk) = p*x1.*(x1.*conj(x1)+params.l1Smooth).^(p/2-1);
+end
