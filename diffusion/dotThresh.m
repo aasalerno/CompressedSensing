446fe87b68a78f7ee9157044c0c844f767a12c25
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-05-13 14:27:52 -0400

    Fixed error in for loop causing i=j pairs from coming up

--- diffusion/dotThresh.m
+++ diffusion/dotThresh.m
@@ -2,28 +2,37 @@ function [locs,wgt] = dotThresh(filename,thresh,sigma)
 
 if nargin<3 || isempty(sigma); sigma = 1; end
 
-if isempty(filename); 
-    v1 = load('C:\Users\saler_000\Dropbox\GradientVector.txt'); 
+if isempty(filename);
+    v1 = load('C:\Users\saler_000\Dropbox\GradientVector.txt');
 else
     v1 = load(filename);
 end
 
-if isempty(thresh); 
-    thresh = 0.1; 
+if isempty(thresh);
+    thresh = 0.1;
 end
 
 nv = length(v1);
 cnt = 0; % Start the location for the ordered pairs
 
-for i=1:nv-1
-    for j = 1+1:nv
-        dp = (dot(v1(i,:),v1(j,:)));
-        if abs(dp) >= thresh
-            cnt = cnt + 1;
-            locs(cnt,:) = int8([i j]);
-            vals(cnt) = dp;
+while(1)
+    for i=1:nv-1
+        for j = i+1:nv
+            dp = (dot(v1(i,:),v1(j,:)));
+            if abs(dp) >= thresh
+                cnt = cnt + 1;
+                locs(cnt,:) = int8([i j]);
+                vals(cnt) = dp;
+            end
         end
     end
+    
+    if ~exist('vals','var')
+        warning('Threshold too high. Decreasing by 10%')
+        thresh = 0.9*thresh;
+    else
+        disp(['Final threshold is ' num2str(thresh)])
+        wgt = exp(-(vals.^2-1)./(2.*sigma.^2))';
+        break;
+    end
 end
-
-wgt = exp(-(vals.^2-1)./(2.*sigma.^2))';
