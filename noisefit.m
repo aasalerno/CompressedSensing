6d2f717712f1ed2ce25fa291f7cc5478a7c60064
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-06-29 13:57:44 -0400

    Try-catch loop added, will fit different things to see if it works. Outputs added

--- noisefit.m
+++ noisefit.m
@@ -1,6 +1,7 @@
-function fits = noisefit(data)
+function [fits,h] = noisefit(data)
 % tests if the data fits specific tests. It looks at Lorentzian, gaussian, rayleigh
-plotit = 0;
+
+plotit = 1;
 if numel(size(data)) ~= 2
     error('This isn''t an image of noise')
 end
@@ -10,9 +11,11 @@ if nargin<2
 end
 
 rayl = @(c1,c2,c3,c4,x) c1.*x./c2.*exp(-(x-c3).^2./c2)+c4;
-lor = @(c1,c2,c3,c4,x) c1./((x - c2).^2 + c3)+c4;
+lor = @(c1,c2,c3,c4,x) c1.*c3./((x - c2).^2 + c3)+c4;
+ext = @(c1,c2,c3,c4,x) c1.*exp(c2*(-x-c3)).*exp(-exp(c2*(-x-c3))) + c4;
+mb = @(c1,c2,c3,c4,x) c1.*x.^2.*exp(c2.*((x.^2-c3))) + c4;
 
-mag = abs(double(data(:)));
+mag = (double(data(:)));
 magMin = min(mag);
 magMax = max(mag);
 magVar = var(mag);
@@ -22,12 +25,43 @@ magMean = mean(mag);
 %fits
 SPGauss = [1 magMean sqrt(magVar)];
 SPRay = [1 magVar magMean 0];
-SPLor = [1 magMean magVar 0];
-[magFitGauss,magRGauss] = fit(magRange',magData','gauss1','StartPoint',SPGauss);
-[magFitRay,magRRay] = fit(magRange',magData',rayl,'Lower',[-Inf 0 -Inf -Inf],'StartPoint',SPRay);
-[magFitLor,magRLor] = fit(magRange',magData',lor,'StartPoint',SPLor);
+SPLor = [1 magMean sqrt(magVar)/2 0];
+% SPExt = [1 magMean magVar 0];
+% SPMB = [1 magMean magVar 0];
 
-if plotit
+
+try
+    [magFitGauss,magRGauss] = fit(magRange',magData','gauss1','StartPoint',SPGauss);
+catch
+    magRGauss.adjrsquare = -1e99;
+end
+
+try
+    [magFitRay,magRRay] = fit(magRange',magData',rayl,'Lower',[-Inf 0 -Inf -Inf],'StartPoint',SPRay);
+catch
+    magRRay.adjrsquare = -1e99;
+end
+
+try
+    [magFitLor,magRLor] = fit(magRange',magData',lor,'StartPoint',SPLor);
+catch
+    magRLor.adjrsquare = -1e99;
+end
+% 
+% try
+%     [magFitExt,magRExt] = fit(magRange',magData',ext,'StartPoint',SPExt);
+% catch
+%     magRExt.adjrsquare = -1e99;
+% end
+
+% try
+%     [magFitMB,magRMB] = fit(magRange',magData',mb,'StartPoint',SPMB);
+% catch
+%     magRMB.adjrsquare = -1e99;
+%    
+% end
+
+if plotit || nargout == 2
     figure; hold on;
     bar(magRange,magData);
     h = plot(magFitGauss);
@@ -36,14 +70,25 @@ if plotit
     set(h,'LineWidth',4,'Color','c');
     h = plot(magFitRay);
     set(h,'LineWidth',4,'Color','b');
-    legend('Histogram','Gaussian','Lorentzian','Rayleigh')
+%     h = plot(magFitExt);
+%     set(h,'LineWidth',4,'Color','k');
+%     h = plot(magFitMB);
+%     set(h,'LineWidth',4,'Color','g');
+    legend('Histogram','Gaussian','Lorentzian','Rayleigh');%,'Extreme Value','Maxwell-Boltz')
     title('Noise Magnitude Fits')
+    xlabel('Voxel Intensity')
+    ylabel('Counts')
+    a = axis;
+    ylim([0 a(4)])
 end
-Rs = [magRLor.adjrsquare magRRay.adjrsquare magRGauss.adjrsquare];
-typs = {'Lorentzian','Rayleigh','Gaussian'};
-[~,ind] = max(Rs);
+Rs = [magRLor.adjrsquare magRRay.adjrsquare magRGauss.adjrsquare];% magRExt.adjrsquare];% magRMB.adjrsquare];
+typs = {'Lorentzian','Rayleigh','Gaussian'};%,'Extreme Value','Maxwell-Boltz'};
+[rsq,ind] = max(Rs);
 
 fits.Lor = magRLor;
 fits.Ray = magRRay;
 fits.Gauss = magRGauss;
+%fits.Ext = magRExt;
+%fits.MB = magRMB;
 fits.best = typs{ind};
+fits.adjrsquare = rsq;
