4dd086c249b91abe912277432426003aecbab219
aasalerno <anthony.salerno@mouseimaging.com>
2015-09-30 11:22:59 -0400

    Made a change in order to add in the fact that whether the dataset comes first or second matters for the sign that we need to append in the gradient calculation

--- grads/gDir.m
+++ grads/gDir.m
@@ -8,15 +8,13 @@ N = size(dirPair,1);
 gradDirTot = zeros([size(x,1) size(x,2) N]);
 gradDir = zeros(size(x));
 
-% This takes 5 seconds :'(
-
 for kk = 1:N
     i = dirPair(kk,1);
     j = dirPair(kk,2);
     x1 = squeeze(x(:,:,i));
     x2 = squeeze(x(:,:,j));
     %gradDirTot(:,:,kk) = params.XFM*(2.*wgt(kk).*(params.XFM'*x1-params.XFM'*x2));
-    % ASK BRIAN IF THIS IS OK
+    % Edited for time 
     gradDirTot(:,:,kk) = (2.*wgt(kk).*((x1-x2)));
 end
 
@@ -24,8 +22,16 @@ end
 nums = (1:N)';
 
 for kk = 1:max(dirPair(:))
-    inRow = nums(any(kk == dirPair,2).*nums ~= 0);
-    gradDir(:,:,kk) = sum(gradDirTot(:,:,inRow),3);
+    % inRow = nums(any(kk == dirPair,2).*nums ~= 0);
+    [inRow,inCol] = find(kk == dirPair);
+    gradDirWork = gradDirTot(:,:,inRow);
+    
+    % Check for the times when it's in the second column
+    for i = 1:numel(inCol)
+        if inCol(i) == 2
+            gradDirTot(:,:,i) = -gradDirWork(:,:,i);
+        end
+    end
+    
+    gradDir(:,:,kk) = sum(gradDirWork,3);
 end
-
-% Whole thing takes ~8 seconds.
